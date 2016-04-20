528ae750bc1a7475735c70ab7d705a182b504e72
Fernando <fernandoamontes@gmail.com>
2016-04-20 12:56:11 -0400

    Identified preliminary features to use

--- PreliminaryAnalysis.R
+++ PreliminaryAnalysis.R
@@ -10,41 +10,21 @@ datawithshots <- subset(alldata, is.na(shot_made_flag) == FALSE)
 # datawithshots$game_event_id[datawithshots$game_id==20000012]
 # 
 # library(ggplot2)
-# p <- ggplot(datawithshots, aes(x=action_type,y=combined_shot_type))
-# p + geom_point(aes(colour = shot_made_flag))
+p <- ggplot(datawithshots, aes(x=game_date,y=game_id))
+p + geom_point(aes(colour = shot_made_flag))
 # 
 # p <- ggplot(datawithshots, aes(x=minutes_remaining*60 + seconds_remaining, y=period))
 # p + geom_point(aes(colour = shot_made_flag))
 # 
-
-p <- ggplot(datawithshots, aes(x=loc_x, y=loc_y))
-p + geom_point(aes(colour = shot_made_flag))
+# p <- ggplot(datawithshots, aes(x=loc_x, y=loc_y))
+# p + geom_point(aes(colour = shot_made_flag))
 # 
 # p <- ggplot(datawithshots, aes(x=game_date,y=game_id))
 # p + geom_point(aes(colour = shot_made_flag))
 # 
 # table(datawithshots$game_id)
 
-# avgshotpercentage <- function(thres=0){
-#   newlist<-c()
-#   for(i in 1:length(takenshot)){
-#    if (takenshot[i] > thres){
-#      newlist<-c(newlist,percentmade[i])
-#    }
-#   }
-#   mean(newlist)
-# }
-
-# shotpercentgate<-function(param){
-#   madeshots<-c()
-#   takenshot<-c()
-#   for(x in unique(datawithshots$param)){
-#     madeshots<-c(madeshots,sum(datawithshots$shot_made_flag[datawithshots$param==x]))
-#     takenshot<-c(takenshot,sum(!is.na(datawithshots$shot_made_flag[datawithshots$param==x])))
-#   }
-#   percentmade<-madeshots*100/takenshot
-#   head(percentmade)
-# }
+# Defining new features ----------------------------------------------------
 
 # Defining new feature dayssincestart
 gamedate <- as.vector(unique(datawithshots$game_date))
@@ -55,10 +35,14 @@ dayssincestart <- as.numeric(gamedate - gamedate[1])
 timeleftingame <- (4-ifelse(datawithshots$period>4, 4, datawithshots$period))*15 + datawithshots$minutes_remaining + as.integer(datawithshots$seconds_remaining/60)
 
 # Defining new feature discrete positions
-loc_x_disc <- as.integer(datawithshots$loc_x/10)
-loc_y_disc <- as.integer(datawithshots$loc_y/10)
+min_x <- min(datawithshots$loc_x)
+min_y <- min(datawithshots$loc_y)
+loc_x_disc <- as.integer((datawithshots$loc_x-min_x)/30)
+loc_y_disc <- as.integer((datawithshots$loc_y-min_y)/30)
 
 datawithshots <- cbind(datawithshots, timeleftingame, loc_x_disc, loc_y_disc)
+
+# Creating percentage shots made for one feature ----------------------------
 
 madeshots<-c()
 takenshot<-c()
@@ -66,7 +50,9 @@ takenshot<-c()
 # Decide what parameter to use
 # param<-"action_type"
 # param<-"game_date"
-param<-"timeleftingame"
+# param<-"timeleftingame"
+#param<-"playoffs"
+param<-"matchup"
 
 for(x in unique(datawithshots[[param]])){
     madeshots<-c(madeshots,sum(datawithshots$shot_made_flag[datawithshots[[param]]==x]))
@@ -76,9 +62,10 @@ percentmade<-madeshots*100/takenshot
 head(percentmade)
 
 # Decide what to use on x-axis in the next plot
-x_axis <- as.numeric(unique(datawithshots$action_type))
-x_axis <- dayssincestart
-x_axis <-unique(datawithshots$timeleftingame)
+# x_axis <- as.numeric(unique(datawithshots$action_type))
+# x_axis <- dayssincestart
+# x_axis <-unique(datawithshots$timeleftingame)
+x_axis <-unique(datawithshots$matchup)
 
 library(ggpmisc)
 # Plot of percentage shots done versus dayssincestart 
@@ -88,13 +75,33 @@ my.formula <- y ~ x
 
 ggplot(permade, aes(x=xaxis, y=percenmade)) + 
   geom_point(shape = 1, col="blue") +
-  xlab("Time left in game") +
-  ylab("shot made percentage") +
-   geom_smooth(method=lm, formula = my.formula) +
-   stat_poly_eq(formula = my.formula,
-                eq.with.lhs = "italic(h)~`=`~",
-                eq.x.rhs = "~italic(z)",
-                aes(label = ..eq.label..), 
-                parse = TRUE)  #  (by default includes 95% confidence region)
+  xlab("Matchup") +
+  ylab("shot made percentage") 
+# +
+#    geom_smooth(method=lm, formula = my.formula) +
+#    stat_poly_eq(formula = my.formula,
+#                 eq.with.lhs = "italic(h)~`=`~",
+#                 eq.x.rhs = "~italic(z)",
+#                 aes(label = ..eq.label..), 
+#                 parse = TRUE)  #  (by default includes 95% confidence region)
 
- 
+# Creating percentage shots made for two features ---------------------------
+
+madeshots<-matrix(data = 0, nrow = max(datawithshots$loc_y_disc), ncol = max(datawithshots$loc_x_disc))
+takenshot<-matrix(data = 0, nrow = max(datawithshots$loc_y_disc), ncol = max(datawithshots$loc_x_disc))
+
+param1<-"loc_x_disc"
+param2<-"loc_y_disc"
+
+for(i in 1:dim(datawithshots)[1]){
+  takenshot[datawithshots$loc_y_disc[i], datawithshots$loc_x_disc[i]] = takenshot[datawithshots$loc_y_disc[i], datawithshots$loc_x_disc[i]] + 1
+  if (datawithshots$shot_made_flag[i] == 1) madeshots[datawithshots$loc_y_disc[i], datawithshots$loc_x_disc[i]] = madeshots[datawithshots$loc_y_disc[i], datawithshots$loc_x_disc[i]] + 1
+}
+percentmade<-madeshots*100/takenshot
+percentmade<-ifelse(is.nan(percentmade), 0, percentmade)
+
+library(gplots) # for colorpanel()
+library(lattice) # for levelplot()
+levelplot(percentmade, scales=list(tck=0, x=list(rot=90)), 
+          col.regions=colorpanel(8, "white", "blue"), at=c(0,10,20,30,40,45,50,60,70), 
+          main="Court position shot percentage", xlab="Court length [/3 ft]", ylab="Court width [/3 ft]")

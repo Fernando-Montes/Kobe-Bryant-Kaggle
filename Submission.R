6753d249f215bda70f3aa6f7f1c6388d579bd0c5
Fernando <fernandoamontes@gmail.com>
2016-04-27 14:00:46 -0400

    Modified cost function to log-loss. Did not make a big impact for some reason.

--- Submission.R
+++ Submission.R
@@ -3,7 +3,8 @@
 datawithoutshots <- subset(alldata, is.na(shot_made_flag) == TRUE)
 
 my_sub <- datawithoutshots
-my_sub_prediction_gbm <- predict(my_gbm, my_sub)
+my_sub_prediction_gbm <- predict(my_glm, my_sub, type = "prob")
+my_sub_prediction_gbm <- my_sub_prediction_gbm$X1
 
 # Setting limits to probabilities ----------------------
 my_sub_prediction_gbm_mod <- ifelse(my_sub_prediction_gbm > 1, 1, 
@@ -11,4 +12,4 @@ my_sub_prediction_gbm_mod <- ifelse(my_sub_prediction_gbm > 1, 1,
 
 tobesub <- cbind(my_sub$shot_id, my_sub_prediction_gbm_mod)
 
-write.csv(tobesub, file = "gbm_submission.csv", sep = " ", row.names = FALSE)
+write.csv(tobesub, file = "gbm_submission-2.csv", sep = " ", row.names = FALSE)
